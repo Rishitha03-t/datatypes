c2b4016cb12cb80ca2e692a62cb8a1dd2f54f7fb
Rishitha03-t <tamalapakularishitha@gmail.com>
2025-01-25 16:16:09 +0530

    Add files via upload

---
+++ datatypes.java
@@ -0,0 +1,20 @@
+
+    public class datatypes {
+        public static void main(String[] args) {
+            int a=30;
+            float b=30.5f;
+            double c=23.5;
+            byte d=123;
+            short e=23456;
+            long f=23456789;
+            char g='A';
+            System.out.println(("int value: ")+a);
+            System.out.println(("float value: ")+b);
+            System.out.println(("double value: ")+c);
+            System.out.println(("byte value: ")+d);
+            System.out.println(("short value: ")+e);
+            System.out.println(("long value: ")+f);
+            System.out.println(("char value: ")+g);
+        }
+   }
+
